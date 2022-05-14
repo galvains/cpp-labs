b457ba22dbe3b4f45a6bf9f2aab2408bd09d1aa9
galvains <93620542+galvains@users.noreply.github.com>
2022-05-14 22:47:40 +0300

    complete menu

--- labs_cpp/App.cpp
+++ labs_cpp/App.cpp
@@ -16,7 +16,25 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "Rus");
 
-	int res = laba_12();
+	int number;
+	int res;
+	cout << "¬ведите є лабораторной работы: "; cin >> number;
+
+	switch (number) {
+		case 1:res = laba_1(); break;
+		case 2:res = laba_2(); break;
+		case 3:res = laba_3(); break;
+		case 4:res = laba_4(); break;
+		case 5:res = laba_5(); break;
+		case 6:res = laba_6(); break;
+		case 7:res = laba_7(); break;
+		case 8:res = laba_8(); break;
+		case 9:res = laba_9(); break;
+		case 10:res = laba_10(); break;
+		case 11:res = laba_11(); break;
+		case 12:res = laba_12(); break;
+	}
+		
 	cout << res << endl;
 
 	system("pause");
